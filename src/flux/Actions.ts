20a2ad879602e0f938980d9718d65c511aa3b736
Fellendorf <finn85fellendorf@gmail.com>
2024-06-08 19:32:55 +0300

    Actions

---
+++ src/flux/Actions.ts
@@ -0,0 +1,27 @@
+import Dispatcher from "./Dispatcher";
+import { ITodoItem } from "./TodoStore";
+
+export enum ActionTypes {
+  ADD_TODO,
+  DELETE_TODO,
+}
+
+export interface IAction<DataType> {
+  type: ActionTypes;
+  data: DataType;
+}
+
+export const Actions = {
+  addTodo(todoItem: ITodoItem) {
+    Dispatcher.dispatch({
+      type: ActionTypes.ADD_TODO,
+      data: todoItem,
+    });
+  },
+  deleteTodo(id: number) {
+    Dispatcher.dispatch({
+      type: ActionTypes.DELETE_TODO,
+      data: { id },
+    });
+  },
+};
